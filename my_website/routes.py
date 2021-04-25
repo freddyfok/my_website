23a4cf9862575c9090321aa49be30a81db08b5c2
Freddy Fok <freddyfok1@gmail.com>
2021-04-25 01:18:03 -0400

    refactored with url_for

--- my_website/routes.py
+++ my_website/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from my_website import app
 
+LINKEDIN_LINK = "https://www.linkedin.com/in/freddyfok/"
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", linkedin_link=f"{LINKEDIN_LINK}")
 
 
 @app.route("/score-sheets")
@@ -13,6 +15,6 @@ def score_sheet():
     return render_template("score-sheets-main.html")
 
 
-@app.route("/score-sheets/wizard")
+@app.route("/score-sheets/new-wizard")
 def wizard_score_sheet():
     return render_template("wizard.html")
